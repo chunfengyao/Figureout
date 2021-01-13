04f2e17a05319c759315eb8ec9464eab4ec99f95
姚春锋 <yaocf@189.cn>
2021-01-13 13:43:55 +0800

    Update Figureout.c

--- Figureout.c
+++ Figureout.c
@@ -35,18 +35,20 @@ int wheatherleft(Op p);
 int main() {
 	char a[maxsize+1]= {
 		0
-	};
+	}
+	;
 	int b;
 	b=0;
 	char postb[maxsize+1]= {
 		0
-	};
+	}
+	;
 	int i;
 	printf("请在英文状态下输入表达式，表达式长度为%d个字符以内。\n",maxsize);
 	printf("注：目前不支持输入中有小数，如果输入了小数，会被忽略掉小数点！！！或者，可以修改源码自己处理一下小数点。\n");
 	printf("注：表达式中请勿出现空格结尾记得加上等于号\n");
 	scanf("%s",a);
-	system("cls");
+	//system("cls");
 	printf("以下是您所输入的表达式\n");
 	puts(a);
 	Op q;
@@ -62,7 +64,8 @@ int main() {
 					if(sch(a[i])==sop(gettopop(q))) {
 						Popop(q,&j);
 						continue;
-					};
+					}
+					;
 					Popop(q,&j);
 					postb[b]=j;
 					b++;
@@ -70,38 +73,43 @@ int main() {
 				if(a[i]==')')
 					continue;
 				Pushop(q,a[i]);
-			};
+			}
+			;
 		} else {
 		// TODO 这里并没有对小数进行入栈，需要的话，自行添加并处理一下！！！
-		//while ((a[i]>='0' && a[i]<='9') || (a[i] == '.')) {
-			while (a[i]>='0' && a[i]<='9') {
+		  while ((a[i]>='0' && a[i]<='9') || (a[i] == '.')) {
+			//while (a[i]>='0' && a[i]<='9') {
 				postb[b]=a[i];
 				b++;
 				i++;
-			};
+			}
+			;
 			postb[b]='#';
 			b++;
 			i--;
 		}
-	};
+	}
+	;
 	while(gettopop(q)!='=') {
 		Item j;
 		Popop(q,&j);
 		postb[b]=j;
 		b++;
-	};
+	}
+	;
 	printf("\n以下为该中缀表达式的后缀表达式：\n");
 	int j;
 	puts(postb);
 	Destoryop(q);
 	printf("\n以下是表达式运算结果：\n%.10f\n",workout(postb));
 	return 0;
-};
+}
+;
 stItem workout(Item a[]) {
 	St q;
 	q=Initst();
 	int i;
-	for (i=0;a[i]!='';i++) {
+	for (i=0;a[i]!='\0';i++) {
 		switch (a[i]) {
 			case '+': {
 				stItem j,k;
@@ -137,14 +145,16 @@ stItem workout(Item a[]) {
 				for (s=1;a[i]>='0' && a[i]<='9';s=s*10) {
 					s1=s1*s+(a[i]-'0');
 					i++;
-				};
+				}
+				;
 				Pushst(q,s1);
 				break;
 			}
 		}
 	}
 	return q->num[q->top];
-};
+}
+;
 //op运算符栈
 int wheatherleft(Op p) {
 	int i;
@@ -152,9 +162,11 @@ int wheatherleft(Op p) {
 	for (i;i>=0;i--) {
 		if(p->data[i]=='(')
 			return 0;
-	};
+	}
+	;
 	return 1;
-};
+}
+;
 int sop(Item a) {
 	switch (a) {
 		case '=' : return 0;
@@ -165,7 +177,8 @@ int sop(Item a) {
 		case '/' : return 5;
 		case ')' : return 6;
 	}
-};
+}
+;
 int sch(Item a) {
 	switch (a) {
 		case '=' : return 0;
@@ -176,29 +189,35 @@ int sch(Item a) {
 		case '/' : return 4;
 		case ')' : return 1;
 	}
-};
+}
+;
 Op Initop() {
 	Op p;
 	p=(Op)malloc(sizeof(op));
 	p->top=-1;
 	return p;
-};
+}
+;
 int Pushop(Op p,Item i) {
 	if (p->top==maxsize-1)
 		return 1; else {
 		(p->top)++;
 		p->data[p->top]=i;
 		return 0;
-	};
-};
+	}
+	;
+}
+;
 int Popop(Op p,Item * i) {
 	if(p->top==-1)
 		return 1; else {
 		*i=p->data[p->top];
 		p->top--;
 		return 0;
-	};
-};
+	}
+	;
+}
+;
 void printop(Op p) {
 	int i;
 	i=p->top;
@@ -206,37 +225,46 @@ void printop(Op p) {
 	for (i;i>=0;i--)
 		printf("%c ",p->data[i]);
 	printf("\n");
-};
+}
+;
 Item gettopop(Op p) {
 	//有一定的隐患（需要加入栈空的判断）！！！！！！
 	return p->data[p->top];
-};
+}
+;
 void Destoryop(Op p) {
 	free(p);
-};
+}
+;
 //st运算子栈
 St Initst() {
 	St p;
 	p=(St)malloc(sizeof(st));
 	p->top=-1;
 	return p;
-};
+}
+;
 int Pushst(St p,stItem i) {
 	if (p->top==maxsize-1)
 		return 1; else {
 		(p->top)++;
 		p->num[p->top]=i;
 		return 0;
-	};
-};
+	}
+	;
+}
+;
 int Popst(St p,stItem * i) {
 	if(p->top==-1)
 		return 1; else {
 		*i=p->num[p->top];
 		p->top--;
 		return 0;
-	};
-};
+	}
+	;
+}
+;
 void Destoryst(St p) {
 	free(p);
-};
+}
+;
